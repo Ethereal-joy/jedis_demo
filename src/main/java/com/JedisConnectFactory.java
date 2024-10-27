5489a38ee09e3876ef52cab90114bbeaa77ec5ab
liguangdi <liguangdi02@163.com>
2024-10-28 02:16:51 +0800

    1

---
+++ src/main/java/com/JedisConnectFactory.java
@@ -0,0 +1,24 @@
+package com;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class JedisConnectFactory {
+    private static final JedisPool jedisPoll;
+
+    static {
+        JedisPoolConfig poolConfig =new JedisPoolConfig();
+        poolConfig.setMaxTotal(8);
+        poolConfig.setMaxIdle(8);
+        poolConfig.setMinIdle(0);
+        poolConfig.setMaxWaitMillis(1000);
+
+        jedisPoll = new JedisPool(poolConfig,"192.168.72.128",6379,1000,"Lgd20021221~");
+
+    }
+
+    public static Jedis getJedis(){
+        return jedisPoll.getResource();
+    }
+}
---
+++ src/test/java/test/JedisTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import com.JedisConnectFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import redis.clients.jedis.Jedis;
+
+public class JedisTest {
+    private Jedis jedis;
+    @BeforeEach
+    void setUp(){
+        jedis = JedisConnectFactory.getJedis();
+        jedis.auth("Lgd20021221~");
+        jedis.select(0);
+    }
+    @Test
+    void testString(){
+        String result = jedis.set("name","huge");
+        System.out.println(result);
+        String name = jedis.get("name");
+        System.out.println(name);
+    }
+    @AfterEach
+    void tearDown(){
+        if (jedis!=null){
+            jedis.close();
+        }
+    }
+}
